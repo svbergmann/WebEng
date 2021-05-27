0651a65c17c015a4f7bcd1c135265af5cadbce6e
ProfSchmergmann <sven.bergmann@rwth-aachen.de>
2021-05-27 10:00:38 +0200

    Worked on H08

--- 08/routes/todos.routes.js
+++ 08/routes/todos.routes.js
@@ -1,13 +1,35 @@
 let todoList = require('../modules/todo.model.js')
 const express = require('express')
-var router = express.Router()
-let todos = new todoList()
+const router = express.Router();
+let todos = new todoList();
 
 // GET all todos
 router.get('/', function (req, res) {
     res.type('application/json');
     res.send(todos.getAllTodos());
-})
+});
+
+router.post('/', function (req, res) {
+    res.type('text/plain');
+    res.send(todos.addTodo(req));
+});
+
+router.get('/:id', function (req, res) {
+    res.send(todos.getTodo(req.query.id));
+});
+
+router.delete('/:id', function (req, res) {
+    res.send(todos.removeTodo(req.query.id));
+});
+
+router.patch('/:id', function (req, res) {
+    todos.setDone(req.query.id, true)
+    res.send('Todo with id ' + req.query.id + ' marked as done.');
+});
+
+router.delete('/:done', function (req, res) {
+
+});
 
 //...
 
