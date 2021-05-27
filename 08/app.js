f8db9db2363a85511bf0ee702605643c5520402b
ProfSchmergmann <sven.bergmann@rwth-aachen.de>
2021-05-27 14:23:47 +0200

    Worked on H08

--- 08/app.js
+++ 08/app.js
@@ -1,2 +1,8 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const app = express();
+const todos = require('./routes/todos.routes');
+const port = 3000;
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port + '!');
+});
+app.use('/todos', todos);
--- 08/routes/todos.routes.js
+++ 08/routes/todos.routes.js
@@ -3,6 +3,9 @@ const express = require('express')
 const router = express.Router();
 let todos = new todoList();
 
+router.use(express.json());
+router.use(express.text());
+
 // GET all todos
 router.get('/', function (req, res) {
     res.type('application/json');
@@ -10,27 +13,52 @@ router.get('/', function (req, res) {
 });
 
 router.post('/', function (req, res) {
-    res.type('text/plain');
-    res.send(todos.addTodo(req));
+    res.type('application/json');
+    res.send(todos.getTodo(todos.addTodo(req.body)));
 });
 
 router.get('/:id', function (req, res) {
-    res.send(todos.getTodo(req.query.id));
+    res.type('application/json');
+    console.log(req.params.id);
+    console.log(todos.getTodo(req.params.id));
+    res.send(todos.getTodo(req.params.id));
 });
 
 router.delete('/:id', function (req, res) {
-    res.send(todos.removeTodo(req.query.id));
-});
-
-router.patch('/:id', function (req, res) {
-    todos.setDone(req.query.id, true)
-    res.send('Todo with id ' + req.query.id + ' marked as done.');
+    res.type('application/json');
+    // Ausgabe zum testen
+    if (todos.removeTodo(req.query.id)) {
+        res.send('Todo with id ' + req.query.id + ' removed.');
+    } else {
+        res.send('Could not remove Todo with id ' + req.query.id + '.');
+    }
 });
 
 router.delete('/:done', function (req, res) {
-
+    if (req.query.done === 'true') {
+        todos.clearDone();
+        res.type('application/json');
+        res.send(todos.getAllTodos());
+    }
 });
 
-//...
+router.delete('/', function () {
+    todos.clear();
+});
+
+router.patch('/:id', function (req, res) {
+    res.type('application/json');
+    if (req.body[0].op === 'replace') {
+        if (req.body[0].path === '/done') {
+            if (req.body[0].value === true) {
+                todos.setDone(req.params.id, true);
+                res.send(todos.getTodo(req.params.id));
+            } else {
+                todos.setDone(req.params.id, false);
+                res.send(todos.getTodo(req.params.id));
+            }
+        }
+    }
+});
 
 module.exports = router;
