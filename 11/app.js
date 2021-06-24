bfe1ad371258d57cdffce015793bc7bd440f2d88
ProfSchmergmann <sven.bergmann@rwth-aachen.de>
2021-06-24 14:02:57 +0200

    Worked on H11 and finished Login

--- 11/app.js
+++ 11/app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const port = 3000;
-const users = require('routes/users')
+const users = require('./routes/users')
+
 app.listen(port, function () {
     console.log('Database app listening on port ' + port + '!');
 });
-app.use('/', express.static(path.join(__dirname, 'public'), {index: 'login.html'}))
-app.use('/scripts/mustache', express.static(path.join(__dirname, 'node_modules/mustache')))
+
+app.use('/', express.static(path.join(__dirname, 'public'), {index: 'login.html'}));
+
+app.use('/scripts/mustache', express.static(path.join(__dirname, 'node_modules/mustache')));
+
+app.use('/users', users);
--- 11/routes/users.js
+++ 11/routes/users.js
@@ -1,6 +1,60 @@
 const express = require('express')
+const path = require("path");
+const {QueryTypes} = require("sequelize");
+const {Sequelize} = require("sequelize");
 const router = express.Router();
 
-// Code here
+router.use(express.urlencoded({extended: true}))
+
+router.post('/check_login', async (req, res) => {
+
+    const sequelize = new Sequelize({
+        dialect: 'sqlite',
+        storage: path.join(__dirname, '..', 'db', 'webengDB.db')
+    });
+
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+
+    let result = await sequelize.query(
+        'SELECT passwort FROM users WHERE benutzername = :userName ',
+        {
+            replacements: {userName: req.body.user},
+            type: QueryTypes.SELECT
+        }
+    );
+
+    let userinfos = [];
+
+    result.forEach(user => {
+        userinfos.push({name: req.body.user, password: user.passwort})
+    });
+
+    let userAndPwCorrect = false;
+
+    for (let user in userinfos) {
+        console.log(user);
+        if (user.password === req.body.pw) {
+            userAndPwCorrect = true;
+        }
+    }
+
+    if (userAndPwCorrect) {
+        res.send("Successfully logged in!")
+    } else {
+        res.status(403).send("Wrong login information!")
+    }
+
+    try {
+        await sequelize.close();
+        console.log('Connection has been closed successfully.');
+    } catch (error) {
+        console.error('Unable to close connection to the database:', error);
+    }
+})
 
 module.exports = router;
